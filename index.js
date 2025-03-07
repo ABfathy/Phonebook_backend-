caf16e6b6c2b236d28eebb1d01dcf36601c07e6a
ABfathy <113472069+ABfathy@users.noreply.github.com>
2025-03-07 15:39:44 +0200

    add validation for post and put

--- index.js
+++ index.js
@@ -37,7 +37,10 @@ app.post("/api/persons", (request,response,next)=>{
 
     }) 
 
-    person.save().then(savedPerson => response.json(savedPerson))
+    person
+    .save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
    
 }
 
@@ -56,7 +59,7 @@ app.put("/api/persons/:id",(request,response,next) =>{
             
     }
 
-    Person.findByIdAndUpdate(request.params.id,person,{new: true})
+    Person.findByIdAndUpdate(request.params.id,person,{new: true , runValidators: true , context: 'query'})
         .then(updatedPerson => { if (updatedPerson){
             response.json(updatedPerson)}
         else response.status(404).send({error:"person not found"})})
@@ -125,6 +128,8 @@ const errorHandler = (error,request,response,next) =>{
 
     if(error.name === 'CastError'){
         return response.status(400).send({error: 'malformatted id'})
+    } else if(error.name === 'ValidationError'){
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
